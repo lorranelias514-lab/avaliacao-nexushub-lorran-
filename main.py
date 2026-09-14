055f87bef9dfa683eee7f76eb88b82b4ca3a1366
lorranelias514-lab <lorranelias514@gmail.com>
2026-09-14 13:05:32 +0000

    V1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+# Cadastro da Startup
+startup = {
+    "nome": "CyberPulse Tech",
+    "segmento": "Segurança da Informação",
+    "ano_adesao": "2026"
+}
+solucoes_ativas = ["Firewall IA, Scan de Vulnerabilidades"]
+print("nome:", startup["nome"])
+print("segmento:", startup["segmento"])
+print("primeiro produto:", solucoes_ativas[0])
 
+# Bancadas
+bancadas = [
+    [1,0],
+    [0,1]
+]
+print("bancada n1:", bancadas[0][0])
+print("bancada n2:", bancadas[0][1])
+print("bancada s1:", bancadas[1][0])
+print("bancada s2:", bancadas[1][1])
+print("1 = ocupado e 0 = livre")
